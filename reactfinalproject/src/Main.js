aab055e00cb0f0db72be5f085a7973bd3447de4a
Noam Lasry <noamlasry02@gmail.com>
2020-07-30 10:37:46 +0300

    stam

--- reactfinalproject/src/Main.js
+++ reactfinalproject/src/Main.js
@@ -3,7 +3,6 @@ import React from 'react';
 import FirebaseFunction from './firebaseFunction'
 import {Card,Button} from 'react-bootstrap'
 
-
 class Main extends React.Component{
 
     constructor(){
@@ -21,15 +20,15 @@ class Main extends React.Component{
     {
         console.log(this.state.arrayOfObject)
         return(
-         <div>
+         <div className="main" >
         
            <FirebaseFunction arrayOfObject={this.acceptArrayOfObject.bind(this)}/>
          
            {this.state.arrayOfObject.map(object => (
-            <div key={object.name}>
-              <Card style={{ width: '18rem' ,position:'absolute',left:`${object.positionX}%`,top:`${object.positionTOP}%`}}>
+            <div key={object.name} >
+              <Card border="primary" style={{ width: '18rem' ,position:'absolute',left:`${object.positionX}%`,top:`${object.positionTOP}%`}}>
                 <Card.Img variant="top" src={object.image} width="100" height="100"/>
-                  <Card.Body>
+                  <Card.Body >
                     <Card.Title>{object.name}</Card.Title>
                     <Card.Text>
                      {object.description}
